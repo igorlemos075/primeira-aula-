b9395b4c693780a31dc3dee2a2e1ff497c59668f
terceira aula <igorlemos957@gmail.com>
2026-03-16 22:01:25 -0300

    terceira aula

---
+++ terceira aula.py
@@ -0,0 +1,34 @@
+media_final = float(input("Média final = "))
+if media_final >= 6:
+    print("Aprovado")
+else:
+    print("Reprovado")
+
+
+    # exercicio 1
+
+    vel = int(input("Velocidade do carro em km/h = "))
+    if vel>80:
+        print("Você foi multado.")
+        multa = (vel - 80) * 5
+        print(f"0 valor da multa é R${multa:.2f}.")
+
+
+    # Exercicio 2
+
+    num1 = float(input("Número 1 = "))
+    num2 = float(input("Número 2 = "))
+    num3 = float(input("Número 3 = "))
+
+    if num1 >= num2 and n1 >= n3:
+        maior = n1
+    if n2 >= n1 and n2 >= n3:
+        maior = n2
+    if n3>=n1 and n3>n2:
+        maior = num3
+    if n1<=n2 and n1<=
+    print(f"O maior numero é o {maior:.0f}")
+
+    print(f"Maior = {maior}")
+    print(f"Menor = {menor}")
+
